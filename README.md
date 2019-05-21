89888661ffb553cb1b582fcf99f9d3d90b221c69
landen <landen>
2019-05-21 16:23:35 +0800

    add code

---
+++ README.md
@@ -0,0 +1,5 @@
+# vue-ssr-demo
+
+vue的服务端渲染示例
+
+集成了客户端服务端同存、同构、vuex、vue-router
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-link to="/bar">Goto Bar</router-link> 
+    <router-link to="/foo">Goto Foo</router-link> 
+    <a href="/bar">Goto Bar - ssr</a> 
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  beforeCreate() {
+    console.log('App.vue beforeCreate');
+  },
+
+  created() {
+    console.log('App.vue created');
+  },
+
+  beforeMount() {
+    console.log('App.vue beforeMount');
+  },
+
+  mounted() {
+    console.log('App.vue mounted');
+  }
+}
+</script>
+
